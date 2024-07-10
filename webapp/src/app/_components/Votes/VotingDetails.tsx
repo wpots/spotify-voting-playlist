7a9ba67fd8a161e73d5b9af332d356817594d419
{W}Pots <12676504+wpots@users.noreply.github.com>
2024-07-10 16:02:09 +0200

    fix(ui): dialog after refafctor

--- webapp/src/app/_components/Votes/VotingDetails.tsx
+++ webapp/src/app/_components/Votes/VotingDetails.tsx
@@ -7,13 +7,13 @@ import VotingStack from './VotingStack';
 import VotersList from './VotersList';
 import { IUser } from '@domain/content';
 interface VotingDialogProps {
-  stats: Record<string, any>;
+  details: Record<string, any>;
 }
 
-export default function VotingDetails({ stats }: Readonly<VotingDialogProps>) {
+export default function VotingDetails({ details }: Readonly<VotingDialogProps>) {
   return (
     <Box sx={{ p: '1rem', backgroundColor: grey[200] }}>
-      {stats.voted?.map((member: IUser) => (
+      {details.voted?.map((member: IUser) => (
         <VotersList
           member={member}
           tooltip={member.vote?.comment}
@@ -21,7 +21,7 @@ export default function VotingDetails({ stats }: Readonly<VotingDialogProps>) {
           key={`list-${member.id}`}
         />
       ))}
-      {stats.pending?.map((member: IUser) => (
+      {details.pending?.map((member: IUser) => (
         <VotersList
           member={member}
           feedback={<Typography variant='caption'>...nog geen feedback</Typography>}
